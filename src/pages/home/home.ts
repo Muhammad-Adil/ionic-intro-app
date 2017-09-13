f4c5c3f1d7ee50b7c263793de7ad135576d8f8d3
Muhammad-Adil <adil.muhammad93@gmail.com>
2017-09-13 15:33:37 +0500

    Ionic intro

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AboutPage } from '../about/about';
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  openPage(){
+    this.navCtrl.push(AboutPage);
+    
   }
 
 }
