312dfe2a29e9c82d873a8f64836234bee5feb3a3
Muhammad-Adil <adil.muhammad93@gmail.com>
2017-09-13 19:18:54 +0500

    AngularFire2 Configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,25 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+//import { environment } from '../environments/environment';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
 import { RegisterPage } from '../pages/register/register';
-
 //import { AboutPage } from '../pages/about/about';
 
+const FirebaseAuth = {  
+    apiKey: "AIzaSyCRK13ipCkjrzARJ6SnAmRBpvAlJJfSB7M",
+    authDomain: "socialapp-b1682.firebaseapp.com",
+    databaseURL: "https://socialapp-b1682.firebaseio.com",
+    projectId: "socialapp-b1682",
+    storageBucket: "socialapp-b1682.appspot.com",
+    messagingSenderId: "731840671009"
+};
 
 @NgModule({
   declarations: [
@@ -20,7 +32,9 @@ import { RegisterPage } from '../pages/register/register';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FirebaseAuth)
+  
   ],
   bootstrap: [IonicApp],
   entryComponents: [
