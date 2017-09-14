4758969b6470e7c42c946ec5b6c2766f0825a416
Muhammad-Adil <adil.muhammad93@gmail.com>
2017-09-14 14:49:43 +0500

    LoggedIn Page added after login functionality with firebase auth

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,8 @@
 import { Component, ViewChild } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 
+import { AngularFireAuth } from 'angularfire2/auth';
+import { LoggedInPage } from '../logged-in/logged-in' ;
 
 /**
  * Generated class for the LoginPage page.
@@ -19,14 +21,34 @@ export class LoginPage {
   @ViewChild('username') name;
   @ViewChild('userpassword') password;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private alertCtrl: AlertController, private afAuth: AngularFireAuth, public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
   }
 
+  alert(message: string){
+    this.alertCtrl.create({
+      title: 'Info',
+      subTitle: message,
+      buttons: ['OK']
+    }).present();
+  }
+
   signInUser(){
+    this.afAuth.auth.signInWithEmailAndPassword(this.name.value, this.password.value)    
+    .then(data => {
+      console.log('got data', this.afAuth.auth.currentUser);
+      //user logged in
+      this.alert('Success! you\'re logged in');
+      this.navCtrl.setRoot( LoggedInPage );
+    })
+    .catch( error => {
+      console.log('got an error' , error );
+      this.alert(error.message);
+    });
+
     console.log( this.name.value , this.password.value );
   }
 }
